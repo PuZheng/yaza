2a7e337db5735d9c68541e54b1a41f0f9ab16f31
xiechao06 <xiechao06@gmail.com>
2014-01-16 20:07:59 +0800

    foo

--- flask_start/__init__.py
+++ flask_start/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 import os
 import posixpath
-from plumbum import cmd
+from plumbum import cmd, local
 
 
 def make_common_project(dir_name):
@@ -17,23 +17,29 @@ def make_common_project(dir_name):
         if project_name:
             break
     while True:
-        add_to_github = raw_input('do you want add this project to github? '
-                                  '[y or n] ')
-        add_to_github = add_to_github.strip()
-        if add_to_github == 'y' or 'n':
-            add_to_github = add_to_github == 'y'
+        host_in_github = raw_input('is this project hosted in a github '
+                                   'repository? note, this repository must be '
+                                   'empty [y or n] ')
+        host_in_github = host_in_github.strip()
+        if host_in_github == 'y' or 'n':
+            host_in_github = host_in_github == 'y'
             break
-    if add_to_github:
+    if host_in_github:
         while True:
             github_repos = raw_input('please input the github repository '
                                      'link: ')
             github_repos = github_repos.strip()
             if github_repos:
                 break
-        # TODO add to github
-
 
     this_dir, this_filename = os.path.split(__file__)
-    import pudb; pudb.set_trace()
-
     cmd.cp['-r', posixpath.join(this_dir, 'templates', 'common'), dir_name]()
+    if host_in_github:
+        add_to_github(dir_name, github_repos)
+
+
+def add_to_github(dir_name, github_repos):
+    with local.cwd(dir_name):
+        cmd.git['init']()
+        cmd.git['remote', 'add', 'origin', github_repos]()
+        cmd.git['pull', 'origin', 'master']()
---
+++ flask_start/templates/common/__foo__/perm.py
@@ -0,0 +1,4 @@
+# -*- coding: UTF-8 -*-
+from flask.ext.principal import ItemNeed, ActionNeed
+# put you needs here
+
