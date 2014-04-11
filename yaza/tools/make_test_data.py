e714c78c89a0069a808ad40bca1d909b7a4efb6d
abc549825 <abc549825@163.com>
2014-04-11 16:17:04 +0800

    fix #1 TODO

--- yaza/tools/make_test_data.py
+++ yaza/tools/make_test_data.py
@@ -13,7 +13,6 @@ from yaza.utils import do_commit
 
 
 class InitializeTestDB(Command):
-
     def initialize_options(self):
         """init options"""
         pass
@@ -24,22 +23,27 @@ class InitializeTestDB(Command):
 
     def run(self):
         from yaza.tools import build_db
+
         build_db.build_db()
         group = do_commit(Group(name="Sample Group"))
         do_commit(User(name="foo",
                        password=generate_password_hash('foo', 'pbkdf2:sha256'),
                        group=group))
-        self.add_sku()
+        self.add_sku(u"T恤", color=u"blue", pic=u"tshirt_1.jpg")
 
-    def add_sku(self):
-        spu = SPU(brief=u"测试", shape=u"T恤")
+        self.add_sku(u"长袖", color=u"red", pic=u"jean_1.jpg")
+
+    def add_sku(self, shape, color, pic):
+        spu = SPU(brief=u"测试用" + shape, shape=shape)
         do_commit(spu)
-        ocspu = OCSPU(spu_id=spu.id, color=u"blue", pic="tshirt_1.jpg")
+        ocspu = OCSPU(spu_id=spu.id, color=color, pic=pic)
         do_commit(ocspu)
         skc = SKC(size=1, ocspu_id=ocspu.id)
         do_commit(skc)
         do_commit(SKU(skc_id=skc.id))
 
+
 if __name__ == "__main__":
     from distutils.dist import Distribution
+
     InitializeTestDB(Distribution()).run()
