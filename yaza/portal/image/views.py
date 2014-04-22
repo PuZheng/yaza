c0227349c0a487e47e0da1599ab4d8d35154e979
abc549825 <abc549825@163.com>
2014-04-22 15:10:36 +0800

    fix #12

--- yaza/portal/image/views.py
+++ yaza/portal/image/views.py
@@ -12,7 +12,7 @@ from yaza.utils import random_str
 def upload():
     fs = request.files['files[]']
     filename = random_str(32) + '.' + fs.filename.split('.')[-1]
-    fs.save(os.path.join(app.config['UPLOADS_DEFAULT_DEST'], filename))
+    fs.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     return jsonify({
         'status': 'success',
         'filename': url_for('image.serve', filename=filename)
@@ -21,4 +21,22 @@ def upload():
 
 @image.route("/serve/<filename>")
 def serve(filename):
-    return send_from_directory(app.config['UPLOADS_DEFAULT_DEST'], filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@image.route("/edges/<int:design_region_id>")
+def detect_edges(design_region_id):
+    from yaza.models import DesignRegion
+    from yaza.apis import wraps
+
+    design_region = DesignRegion.query.get_or_404(design_region_id)
+    return jsonify(wraps(design_region).edges)
+
+
+@image.route("/control-points/<int:design_region_id>")
+def calc_control_points(design_region_id):
+    from yaza.models import DesignRegion
+    from yaza.apis import wraps
+
+    design_region = DesignRegion.query.get_or_404(design_region_id)
+    return jsonify(wraps(design_region).control_points)
