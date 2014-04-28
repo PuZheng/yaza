5b29461aab1035e8faf8432021d80b85b33221df
xiechao06 <xiechao06@gmail.com>
2014-04-28 11:29:19 +0800

    fix #36

--- yaza/static/js/spu/main.js
+++ yaza/static/js/spu/main.js
@@ -13,12 +13,13 @@ require.config({
         backbone: 'components/backbone/backbone',
         bootstrap: 'components/bootstrap/dist/js/bootstrap.min',
         handlebars: 'components/handlebars/handlebars.amd.min',
+        'underscore.string': 'components/underscore.string/dist/underscore.string.min',
         'jquery-file-upload': 'components/blueimp-file-upload/js/jquery.fileupload',
         'jquery.ui.widget': 'components/jquery-ui/ui/minified/jquery.ui.widget.min',
         'jquery.iframe-transport': 'components/jquery.iframe-transport/jquery.iframe-transport',
         'text': 'components/text/text',
         'cookies-js': 'components/cookies-js/src/cookies.min',
-        'select2':['//cdn.staticfile.org/select2/3.4.6/select2.min', 'components/select2/select2.min'],
+        'select2': 'components/select2/select2.min',
         // application
         dispatcher: 'js/dispatcher',
         app: 'js/spu/app',
@@ -30,6 +31,9 @@ require.config({
     shim: {
         'underscore': {
             exports: '_',
+        },
+        'underscore.string': {
+            deps: ['underscore'],
         },
         'backbone': {
             deps: ['jquery', 'underscore'],
--- yaza/static/js/spu/views/jit-preview.js
+++ yaza/static/js/spu/views/jit-preview.js
@@ -1,17 +1,70 @@
 (function (mods) {
-    define(mods, function (Backbone, dispatcher, handlebars, jitPreviewTemplate) {
+    define(mods, function (_, Backbone, dispatcher, Handlebars, jitPreviewTemplate) {
+        _.mixin(_.str.exports());
+        Handlebars.default.registerHelper("eq", function (target, source, options) {
+            if (target === source) {
+                return options.fn(this);
+            } else {
+                return options.inverse(this);
+            }
+        });
+
         var JitPreview = Backbone.View.extend({
-            _template: handlebars.default.compile(jitPreviewTemplate),
+            _template: Handlebars.default.compile(jitPreviewTemplate),
 
             initialize: function (options) {
                 this._spu = options.spu; 
             },
+            
+            events: {
+                'click .ocspu-selector .thumbnail': function (evt) {
+                    this.$(".ocspu-selector .thumbnail").removeClass("selected");
+                    $(evt.currentTarget).addClass("selected");
+                    // show aspects
+                    this.$('.aspect-selector').empty();
+                    var ocspu = $(evt.currentTarget).data('ocspu');
+                    ocspu.aspectList.forEach(function (aspect) {
+                        $(this._aspectTpl({aspect: aspect})).appendTo(this.$('.aspect-selector')).data('aspect', aspect);
+                    }.bind(this));
+                    this.$('.aspect-selector .thumbnail:first-child').click();
+                },
+                'click .aspect-selector .thumbnail': function (evt) {
+                    this.$(".aspect-selector .thumbnail").removeClass("selected");
+                    $(evt.currentTarget).addClass("selected");
+                    // show hotspot
+                    var aspect = $(evt.currentTarget).data('aspect');
+                    this.$('.hotspot img').attr('src', aspect.picUrl);
+                    
+                    aspect.designRegionList.forEach(function (designRegion) {
+                        var select = this.$('select[name="current-design-region"]');
+                        $(_.sprintf('<option value="%d">%s</option>', designRegion.id, 
+                                designRegion.name)).appendTo(select);
+                    }.bind(this));
+                    $('select[name="current-design-region"]').change(
+                            function (designRegionList) {
+                                return function (evt) {
+                                    for (var i = 0; i < designRegionList.length; ++i) {
+                                        var designRegion = designRegionList[i];
+                                        if (designRegion.id == $(this).val()) {
+                                            dispatcher.trigger('design-region-selected',
+                                                designRegion);
+                                            break;
+                                        }
+                                    }
+                                }
+                            }(aspect.designRegionList)).change();
+                },
+            },
+
             render: function () {
-                // TODO load images
-                this.$el.append(this._template());
-                dispatcher.trigger('design-region-selected', this._spu.ocspuList[0].aspectList[0].designRegionList[0]);
+                this.$el.append(this._template({spu: this._spu}));
+                this._aspectTpl = Handlebars.default.compile(this.$('script.thumbnail').html().trim());
+                this.$('.ocspu-selector .thumbnail').each(function (idx, e) {
+                    $(e).data('ocspu', this._spu.ocspuList[idx]);
+                }.bind(this));
+                this.$('.ocspu-selector .thumbnail:first-child').click();
             },
         });
         return JitPreview;
     });
-})(['backbone', 'dispatcher', 'handlebars','text!templates/jit-preview.hbs']);
+})(['underscore', 'backbone', 'dispatcher', 'handlebars','text!templates/jit-preview.hbs', 'underscore.string']);
