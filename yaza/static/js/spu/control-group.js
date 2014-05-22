4fd3f58f386623193de16b7b3222299e9d662c21
abc549825 <abc549825@163.com>
2014-05-22 11:01:53 +0800

    fix #66 yaza-66 优化定制区域的图片缩放

--- yaza/static/js/spu/control-group.js
+++ yaza/static/js/spu/control-group.js
@@ -1,5 +1,8 @@
 define(function () {
+    var radius = 4;
+
     function makeControlGroup(node, title, uuid, resizable) {
+
         resizable = !!resizable;
         var group = new Kinetic.Group({
             x: node.x() - node.offsetX() + node.width() / 2,
@@ -8,13 +11,13 @@ define(function () {
             name: title,
             uuid: uuid
         });
-        group.on('dragstart', function() {
+        group.on('dragstart', function () {
             this.moveToTop();
         });
-        group.on('dragend', function() {
+        group.on('dragend', function () {
             node.position({
                 x: group.x(),
-                y: group.y(),
+                y: group.y()
             });
         });
         var rect = new Kinetic.Rect({
@@ -25,7 +28,7 @@ define(function () {
             width: node.width(),
             height: node.height(),
             dash: [5, 5],
-            name: 'rect',
+            name: 'rect'
         });
         rect.on("mouseover",function () {
             document.body.style.cursor = 'move';
@@ -39,9 +42,9 @@ define(function () {
         var line = new Kinetic.Line({
             stroke: 'gray',
             strokeWidth: 1,
-            points: [0, - (node.height() / 2 + 50), 0, 0],
+            points: [0, -(node.height() / 2 + 50), 0, 0],
             dash: [5, 5],
-            name: 'handle-bar-line',
+            name: 'handle-bar-line'
         });
         group.add(line);
         var circle = new Kinetic.Circle({
@@ -49,7 +52,7 @@ define(function () {
             y: 0,
             fill: 'red',
             radius: 3,
-            name: 'center',
+            name: 'center'
         });
         group.add(circle);
         if (resizable) {
@@ -57,9 +60,23 @@ define(function () {
             _addAnchor(group, node.width() / 2, -node.height() / 2, 'topRight', node, "ne-resize");
             _addAnchor(group, node.width() / 2, node.height() / 2, 'bottomRight', node, "nw-resize");
             _addAnchor(group, -node.width() / 2, node.height() / 2, 'bottomLeft', node, "ne-resize");
+
+            _addRect(group, -radius, -node.height() / 2 - radius, 'top', node, "s-resize", function (pos) {
+                return {x: group.getX() - radius, y: pos.y};
+            });
+            _addRect(group, -node.height() / 2 - radius, -radius, 'left', node, "w-resize", function (pos) {
+                return {x: pos.x, y: group.getY() - radius};
+            });
+
+            _addRect(group, -radius, node.height() / 2 - radius, 'bottom', node, "s-resize", function (pos) {
+                return {x: group.getX() - radius, y: pos.y};
+            });
+            _addRect(group, node.height() / 2 - radius, -radius, 'right', node, "w-resize", function (pos) {
+                return {x: pos.x, y: group.getY() - radius};
+            });
         }
         _addRotationHandleBar(group, 0,
-                -(node.height() / 2 + 50), 'handleBar', node);
+            -(node.height() / 2 + 50), 'handleBar', node);
 
         return group;
     }
@@ -75,7 +92,7 @@ define(function () {
             radius: 7,
             name: name,
             draggable: true,
-            dragOnTop: false,
+            dragOnTop: false
         });
         anchor.on('mouseover', function () {
             var layer = this.getLayer();
@@ -107,12 +124,12 @@ define(function () {
             var offsetY = rect.y() + rect.height() / 2;
             group.move({
                 x: offsetX,
-                y: offsetY,
+                y: offsetY
             });
             group.getChildren().each(function (node) {
                 node.move({
                     x: -offsetX,
-                    y: -offsetY,
+                    y: -offsetY
                 });
             });
             group.getLayer().draw();
@@ -120,7 +137,56 @@ define(function () {
         });
 
         group.add(anchor);
-    };
+    }
+
+    function _addRect(group, x, y, name, node, cursorStyle, dragBoundFunc) {
+        var rect = new Kinetic.Rect({
+            x: x,
+            y: y,
+            name: name,
+            width: 8,
+            height: 8,
+            stroke: '#666',
+            fill: '#ddd',
+            strokeWidth: 2,
+            radius: 7,
+            draggable: true,
+            dragOnTop: false,
+            dragBoundFunc: dragBoundFunc
+        });
+        rect.on("mouseover",function () {
+            document.body.style.cursor = cursorStyle;
+            this.strokeWidth(4);
+            this.getLayer().draw();
+        }).on("mouseout",function () {
+            document.body.style.cursor = "default";
+            this.strokeWidth(2);
+            this.getLayer().draw();
+        }).on('mousedown touchstart',function () {
+            group.setDraggable(false);
+            this.moveToTop();
+        }).on('dragmove',function () {
+            _updateControlGroup(this, node);
+        }).on('dragend', function () {
+            // 重新计算group的位置, 以保证始终能按照物理中心进行旋转
+            var rect = group.find('.rect')[0];
+            var offsetX = rect.x() + rect.width() / 2;
+            var offsetY = rect.y() + rect.height() / 2;
+            group.move({
+                x: offsetX,
+                y: offsetY
+            });
+            group.getChildren().each(function (node) {
+                node.move({
+                    x: -offsetX,
+                    y: -offsetY
+                });
+            });
+            group.getLayer().draw();
+            group.setDraggable(true);
+        });
+        group.add(rect);
+    }
 
     function _addRotationHandleBar(group, x, y, name, node) {
         var anchor = new Kinetic.Circle({
@@ -131,26 +197,26 @@ define(function () {
             strokeWidth: 2,
             radius: 6,
             name: name,
-            draggable: true,
+            draggable: true
         });
-        anchor.on('mouseover', function() {
+        anchor.on('mouseover', function () {
             var layer = this.getLayer();
             document.body.style.cursor = 'pointer';
-            this.setStrokeWidth(4);
+            this.strokeWidth(4);
             layer.draw();
         });
-        anchor.on('mouseout', function() {
+        anchor.on('mouseout', function () {
             var layer = this.getLayer();
             document.body.style.cursor = 'default';
             this.strokeWidth(2);
             layer.draw();
         });
-        anchor.on('mousedown touchstart', function() {
+        anchor.on('mousedown touchstart', function () {
             group.setDraggable(false);
             this.moveToTop();
         });
 
-        anchor.on('dragmove', function() {
+        anchor.on('dragmove', function () {
             var dx = this.x();
             var dy = this.y();
             var line = group.find('.handle-bar-line');
@@ -180,6 +246,11 @@ define(function () {
         var bottomRight = group.find('.bottomRight')[0];
         var bottomLeft = group.find('.bottomLeft')[0];
 
+        var top = group.find(".top")[0];
+        var right = group.find(".right")[0];
+        var bottom = group.find(".bottom")[0];
+        var left = group.find(".left")[0];
+
         var anchorX = anchor.x();
         var anchorY = anchor.y();
 
@@ -192,10 +263,17 @@ define(function () {
             case 'topLeft':
                 rect.position({
                     x: anchorX,
-                    y: anchorY,
+                    y: anchorY
                 });
                 topRight.y(anchorY);
                 bottomLeft.x(anchorX);
+
+                top.x((topLeft.x() + topRight.x()) / 2 - radius);
+                top.y(topLeft.y() - radius);
+                bottom.x((bottomLeft.x() + bottomRight.x()) / 2 - radius);
+                left.x(topLeft.x() - radius);
+                left.y((topLeft.y() + bottomLeft.y()) / 2 - radius);
+                right.y(left.y());
 
                 var newWidth = topRight.x() - topLeft.x();
                 var newHeight = bottomLeft.y() - topLeft.y();
@@ -206,27 +284,107 @@ define(function () {
                 rect.y(anchorY);
                 topLeft.y(anchorY);
                 bottomRight.x(anchorX);
-                var newWidth = topRight.x() - topLeft.x();
-                var newHeight = bottomLeft.y() - topLeft.y();
-                var offsetX = -(oldWidth - newWidth) / 2;
-                var offsetY = (oldHeight - newHeight) / 2;
+
+                top.x((topLeft.x() + topRight.x()) / 2 - radius);
+                top.y(topLeft.y() - radius);
+                bottom.x((bottomLeft.x() + bottomRight.x()) / 2 - radius);
+                left.y((topRight.y() + bottomRight.y()) / 2 - radius);
+                right.y(left.y());
+                right.x(topRight.x() - radius);
+
+                newWidth = topRight.x() - topLeft.x();
+                newHeight = bottomLeft.y() - topLeft.y();
+                offsetX = -(oldWidth - newWidth) / 2;
+                offsetY = (oldHeight - newHeight) / 2;
                 break;
             case 'bottomRight':
                 topRight.x(anchorX);
                 bottomLeft.y(anchorY);
-                var newWidth = topRight.x() - topLeft.x();
-                var newHeight = bottomLeft.y() - topLeft.y();
-                var offsetX = -(oldWidth - newWidth) / 2;
-                var offsetY = -(oldHeight - newHeight) / 2;
+
+                top.x((topLeft.x() + topRight.x()) / 2 - radius);
+                bottom.x((bottomLeft.x() + bottomRight.x()) / 2 - radius);
+                bottom.y(bottomRight.y() - radius);
+                right.x(bottomRight.x() - radius);
+                right.y((topRight.y() + bottomRight.y()) / 2 - radius);
+                left.y(right.y());
+
+                newWidth = topRight.x() - topLeft.x();
+                newHeight = bottomLeft.y() - topLeft.y();
+                offsetX = -(oldWidth - newWidth) / 2;
+                offsetY = -(oldHeight - newHeight) / 2;
                 break;
             case 'bottomLeft':
                 rect.x(anchorX);
                 topLeft.x(anchorX);
                 bottomRight.y(anchorY);
-                var newWidth = topRight.x() - topLeft.x();
-                var newHeight = bottomLeft.y() - topLeft.y();
-                var offsetX = (oldWidth - newWidth) / 2;
-                var offsetY = -(oldHeight - newHeight) / 2;
+
+                top.x((topLeft.x() + topRight.x()) / 2 - radius);
+                bottom.x((bottomLeft.x() + bottomRight.x()) / 2 - radius);
+                bottom.y(bottomRight.y() - radius);
+                left.x(bottomLeft.x() - radius);
+                left.y((topRight.y() + bottomRight.y()) / 2 - radius);
+                right.y(left.y());
+
+                newWidth = topRight.x() - topLeft.x();
+                newHeight = bottomLeft.y() - topLeft.y();
+                offsetX = (oldWidth - newWidth) / 2;
+                offsetY = -(oldHeight - newHeight) / 2;
+                break;
+            case 'right':
+                topRight.x(right.x() + radius);
+                bottomRight.x(right.x() + radius);
+                top.x((topLeft.x() + topRight.x()) / 2 - radius);
+                bottom.x((bottomLeft.x() + bottomRight.x()) / 2 - radius);
+
+                newWidth = right.x() - left.x();
+                newHeight = oldHeight;
+
+                offsetX = (newWidth - oldWidth) / 2;
+                offsetY = 0;
+                break;
+            case 'left':
+                topLeft.x(left.x() + radius);
+                bottomLeft.x(topLeft.x());
+                top.x((topLeft.x() + topRight.x()) / 2 - radius);
+                bottom.x((bottomLeft.x() + bottomRight.x()) / 2 - radius);
+
+                rect.x(topLeft.x());
+
+                newWidth = right.x() - left.x();
+                newHeight = oldHeight;
+
+                offsetX = (oldWidth - newWidth) / 2;
+                offsetY = 0;
+                break;
+            case 'top':
+                topLeft.y(top.y() + radius);
+                topRight.y(topLeft.y());
+
+                left.y((topLeft.y() + bottomLeft.y()) / 2 - radius);
+                right.y((topRight.y() + bottomRight.y()) / 2 - radius);
+
+                rect.y(topLeft.y());
+
+                newWidth = oldWidth;
+                newHeight = bottom.y() - top.y();
+
+                offsetX = 0;
+                offsetY = (oldHeight - newHeight) / 2;
+
+                break;
+            case 'bottom':
+                bottomLeft.y(bottom.y() + radius);
+                bottomRight.y(bottomLeft.y());
+
+                left.y((topLeft.y() + bottomLeft.y()) / 2 - radius);
+                right.y((topRight.y() + bottomRight.y()) / 2 - radius);
+
+                newWidth = oldWidth;
+                newHeight = bottom.y() - top.y();
+
+                offsetX = 0;
+                offsetY = (newHeight - oldHeight ) / 2;
+
                 break;
         }
 
@@ -234,20 +392,20 @@ define(function () {
         // 注意, 移动node, x, y设定在了物理中心
         node.size(rect.size()).move({
             x: offsetX,
-            y: offsetY,
+            y: offsetY
         }).offset({
             x: newWidth / 2,
-        y: newHeight / 2,
+            y: newHeight / 2
         });
         ['.handleBar', '.handle-bar-line', '.center'].forEach(function (nodeName) {
             group.find(nodeName)[0].move({
                 x: offsetX,
-                y: offsetY,
+                y: offsetY
             });
         });
         group.getLayer().draw();
         node.getLayer().draw();
-    } 
+    }
 
     return makeControlGroup;
 });
