28269f6990936de66d91c560df9611feb84f86f4
abc549825 <abc549825@163.com>
2014-05-30 09:57:58 +0800

    fix #136 yaza-136 SPU上传后, 需要将图片推送到又拍云.

--- yaza/upyun_handler/handler.py
+++ yaza/upyun_handler/handler.py
@@ -13,6 +13,8 @@ PASSWORD = "xiechao123456"
 
 rootpath = "/upload-demo"
 
+headers = {"x-gmkerl-type": "fix_max", "x-gmkerl-value": 960}
+
 try:
     import requests
 except ImportError:
@@ -27,10 +29,14 @@ def alt_path(file_):
 
 
 def upload_image(file_, new_name):
-    file_name = alt_path(os.path.join(rootpath, file_))
+    file_name = alt_path(os.path.join(rootpath, new_name))
     up = upyun.UpYun(BUCKETNAME, USERNAME, PASSWORD, timeout=30, endpoint=upyun.ED_AUTO)
+
+    #删除原有文件，重新上传
+    up.delete(file_name)
+
     with open(file_, "rb") as f:
-        res = up.put(file_name, f, checksum=False)
+        res = up.put(file_name, f, checksum=False, headers=headers)
         if res:
             return file_name
     return None
@@ -41,4 +47,5 @@ def parse_image(file_):
 
 
 if __name__ == "__main__":
-    print upload_image(r"C:\Users\Young\Pictures\1\dahuaxiyou.jpg", "1234\dahuaxiyou.jpg")
+    a = upload_image(r"C:\Users\Public\Pictures\Sample Pictures\Jellyfish.jpg", r"2\nga.jpg")
+    print parse_image(a)
