f10a42b646eb016acb65648c00ac419a884258e1
abc549825 <abc549825@163.com>
2014-07-16 11:31:51 +0800

    fix #258

--- yaza/fabfile.py
+++ yaza/fabfile.py
@@ -4,56 +4,55 @@ import os
 from fabric.api import *
 from flask import json
 
-import yaza
-
 config = json.load(file(os.path.join(os.path.split(__file__)[0], "fab.json")))
 
 env.hosts = config["hosts"]
 env.passwords = config["passwords"]
 env.user = config["user"]
+branch = config.get("branch", "master")
+
+yaza_env = "/srv/www/yaza-env"
+dist_dir = "yaza/yaza/static/dist"
 
 
 def prepare_deploy():
-    branch = "master"
-    if "branch in env:":
-        branch = env.branch
-    with cd("/srv/www/yaza-env"):
+    with cd(yaza_env):
         sudo('cd yaza && git pull origin %s' % branch, user="www-data")
         sudo("r.js -o build.js", user="www-data")
 
 
 def upload():
-    base_dir = os.path.join(os.path.split(yaza.__file__)[0], "static\dist")
-    included_dirs = [os.path.normpath(os.path.join(base_dir, d)) for d in config["include"].get("dir", [])]
-    included_files = [os.path.normpath(os.path.join(base_dir, f)) for f in config["include"].get("file", [])]
+    with cd(yaza_env):
+        included_dirs = [os.path.normpath(os.path.join(dist_dir, d)) for d in config["include"].get("dir", [])]
+        included_files = [os.path.normpath(os.path.join(dist_dir, f)) for f in config["include"].get("file", [])]
 
-    excluded_dirs = [os.path.normpath(os.path.join(base_dir, d)) for d in config["exclude"].get("dir", [])]
+        excluded_dirs = [os.path.normpath(os.path.join(dist_dir, d)) for d in config["exclude"].get("dir", [])]
+        excluded_files = [os.path.normpath(os.path.join(dist_dir, f)) for f in config["exclude"].get("file", [])]
 
-    excluded_files = [os.path.normpath(os.path.join(base_dir, f)) for f in config["exclude"].get("file", [])]
+        for d in included_dirs:
+            for root, dir, files in os.walk(d):
+                if root not in excluded_dirs:
+                    for file_ in files:
+                        if os.path.join(root, file_) not in excluded_files:
+                            upload_file(os.path.join(root, file_))
 
-    for d in included_dirs:
-        for root, dir, files in os.walk(d):
-            if root not in excluded_dirs:
-                for file_ in files:
-                    if os.path.join(root, file_) not in excluded_files:
-                        upload_file(os.path.join(root, file_))
-
-    for f in included_files:
-        upload_file(f)
+        for f in included_files:
+            upload_file(f)
 
 
 def upload_file(file_):
     from yaza.qiniu_handler import upload_text
     from yaza.basemain import app
+
     print "uploading ..." + file_
-    file_name = os.path.relpath(file_, os.path.join(os.path.split(yaza.__file__)[0], "static\dist"))
+    file_name = os.path.relpath(file_, yaza_env + dist_dir)
     upload_text(file_name, open(file_).read(), app.config["QINIU_CONF"]["SPU_IMAGE_BUCKET"])
 
 
 def deploy():
     prepare_deploy()
     upload()
-    with cd("/srv/www/yaza-env"):
+    with cd(yaza_env):
         with prefix('source env/bin/activate'):
             run('python -c "import sys; print sys.path"')
             sudo("cd yaza && pip install -r requirements.txt && python setup.py install", user="www-data")
