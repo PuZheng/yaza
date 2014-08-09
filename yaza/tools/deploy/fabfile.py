ac6e2ae9d03d43cc006373979e614478d987c7e3
xiechao06 <xiechao06@gmail.com>
2014-08-09 20:03:21 +0800

    partial

--- yaza/tools/deploy/fabfile.py
+++ yaza/tools/deploy/fabfile.py
@@ -21,19 +21,28 @@ def prepare_deploy(branch):
                               'git pull origin master'])
         else:
             cmd = '&& '.join(['cd yaza', 'git checkout master',
-                             'eval "$(git branch -D ' + branch + ')"',
+                             'git branch -D ' + branch,
                              'git fetch origin',
                              'git checkout -b ' + branch])
         run(cmd)
         with prefix('source env/bin/activate'):
             run("cd yaza && pip install -r requirements.txt -i http://pypi.douban.com/simple && python setup.py develop")
         run('cd yaza/yaza && bower install')
+
+def build():
+    with cd(yaza_env):
         cmd = '&& '.join([
             'cd yaza/yaza/static/vendor/bootswatch-scss',
             'rbenv local 2.1.2',
             'grunt build:flatly',
         ])
         run(cmd)
+        with cd('yaza/yaza/static/sass'):
+            files = run('find -name "[^_]*.scss"').split('\n')
+            files = [fname.strip().lstrip('./').rstrip('.scss') for
+                     fname in files]
+            for fname in files:
+                run('sass %s.scss ../css/%s.css' % (fname, fname))
 
 def optimization():
     with cd(yaza_env):
@@ -43,7 +52,7 @@ def upload2cdn():
 
     with cd(yaza_env):
         with prefix('source env/bin/activate'):
-            run('cd yaza/yaza && cat tools/upload2cdn-files.txt | python tools/upload2cdn.py')
+            run('cd yaza/yaza && cat tools/deploy/upload2cdn-files.txt | python tools/deploy/upload2cdn.py')
 
 
 def deploy(branch='master'):
