f62f85a3217e77e5a71cd15a6a85797009210f72
xiechao06 <xiechao06@gmail.com>
2014-08-09 23:52:40 +0800

    fix #258

--- yaza/tools/deploy/fabfile.py
+++ yaza/tools/deploy/fabfile.py
@@ -2,7 +2,7 @@
 import os
 import json
 
-from fabric.api import cd, sudo, local, env, prefix, run
+from fabric.api import cd, sudo, local, env, prefix, run, put, prompt
 
 config = json.load(file(os.path.join(os.path.split(__file__)[0],
                                      "conf.json")))
@@ -14,22 +14,26 @@ env.user = config["user"]
 yaza_env = "/srv/www/yaza-env"
 
 
-def prepare_deploy(branch):
+def sync_code(branch):
     with cd(yaza_env):
-        if branch == 'master':
-            cmd = '&& '.join(['cd yaza', 'git checkout master',
-                              'git pull origin master'])
+        s = run('cd yaza && git branch')
+        if branch in [b.lstrip('* ').strip() for b in s.split()]:
+            cmd = '&& '.join(['cd yaza', 'git checkout ' + branch,
+                            'git pull origin ' + branch])
         else:
             cmd = '&& '.join(['cd yaza', 'git checkout master',
-                             'git branch -D ' + branch,
-                             'git fetch origin',
-                             'git checkout -b ' + branch])
+                            'git branch -D ' + branch,
+                            'git fetch origin',
+                            'git checkout ' + branch])
         run(cmd)
+
+def install():
+    with cd(yaza_env):
         with prefix('source env/bin/activate'):
             run("cd yaza && pip install -r requirements.txt -i http://pypi.douban.com/simple && python setup.py develop")
         run('cd yaza/yaza && bower install')
 
-def build():
+def build(rebuild_data):
     with cd(yaza_env):
         cmd = '&& '.join([
             'cd yaza/yaza/static/vendor/bootswatch-scss',
@@ -41,8 +45,14 @@ def build():
             files = run('find -name "[^_]*.scss"').split('\n')
             files = [fname.strip().lstrip('./').rstrip('.scss') for
                      fname in files]
+            print files
             for fname in files:
                 run('sass %s.scss ../css/%s.css' % (fname, fname))
+        if rebuild_data:
+            if prompt('Warning: Are you sure to rebuild data?[yes/no]') == 'yes':
+                run('cd yaza/yaza; python tools/make_test_data.py')
+            else:
+                print 'DATA REBUILDING ABORTED!'
 
 def optimization():
     with cd(yaza_env):
@@ -54,14 +64,23 @@ def upload2cdn():
         with prefix('source env/bin/activate'):
             run('cd yaza/yaza && cat tools/deploy/upload2cdn-files.txt | python tools/deploy/upload2cdn.py')
 
+def take_effect():
+    remote_config = os.path.join(os.path.split(__file__)[0], "remote-config.py")
+    with cd(yaza_env):
+        put(remote_config, 'yaza/yaza/config.py')
+    env.user = config['sudoer']
+    sudo("service nginx restart", user='root')
+    sudo("service uwsgi restart yaza", user='root')
+    env.user = config['user']
 
-def deploy(branch='master'):
-    prepare_deploy(branch)
+
+
+def deploy(branch='master', rebuild_data=False):
+    sync_code(branch)
+    build(rebuild_data)
     optimization()
     upload2cdn()
-
-    sudo("service nginx restart")
-    sudo("service uwsgi restart yaza")
+    take_effect()
 
 
 if __name__ == "__main__":
