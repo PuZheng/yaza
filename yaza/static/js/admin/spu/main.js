6565d27851b3efff3d3b5755339797d8de72faa3
xiechao06 <xiechao06@gmail.com>
2014-08-11 11:00:22 +0800

    fix #258

--- yaza/static/js/admin/spu/main.js
+++ yaza/static/js/admin/spu/main.js
@@ -15,7 +15,6 @@
             handlebars: ['http://cdn.bootcss.com/handlebars.js/2.0.0-alpha.4/handlebars.amd', 'components/handlebars/handlebars.amd.min'],
             'underscore.string': ['http://cdn.bootcss.com/underscore.string/2.3.3/underscore.string.min', 'components/underscore.string/dist/underscore.string.min'],
             'spectrum': ['http://cdn.bootcss.com/spectrum/1.3.0/js/spectrum.min', 'components/spectrum/spectrum'],
-            'fancybox': 'http://cdn.bootcss.com/fancybox/2.1.5/jquery.fancybox.min',
             'jquery.ui.widget': ['http://cdn.bootcss.com/jqueryui/1.10.4/jquery-ui.min', 'components/jquery-ui/ui/jquery.ui.widget'],
             'toastr': 'http://cdn.bootcss.com/toastr.js/latest/js/toastr.min',
             'js-url': 'http://cdn.bootcss.com/js-url/1.8.4/url',
@@ -49,10 +48,6 @@
                 deps: ['css!http://cdn.bootcss.com/spectrum/1.3.0/css/spectrum.min.css', 'jquery'],
                 //deps: ['css!components/spectrum/spectrum.css', 'jquery'],
                 exports: '$.fn.spectrum'
-            },
-            'fancybox': {
-                deps: ['css!http://cdn.bootcss.com/fancybox/2.1.5/jquery.fancybox.min.css', 'jquery'],
-                exports: '$.fn.fancybox'
             },
             'jquery-file-upload': {
                 deps: ['css!components/blueimp-file-upload/css/jquery.fileupload.css', 
