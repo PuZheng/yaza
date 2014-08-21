91b85cdd0886ae0b0eee258ed4f97ef62851dd70
abc549825 <abc549825@163.com>
2014-08-21 16:58:21 +0800

    fix #310
    yaza-310 用户上传的图片要发送到qiniu

--- yaza/qiniu_handler/__init__.py
+++ yaza/qiniu_handler/__init__.py
@@ -1,3 +1,2 @@
 # -*- coding:utf-8 -*-
-from .handler import (upload_image, upload_image_str, delete_file,
-                      AlreadyExists, upload_str, upload_file)
+from .handler import (AlreadyExists, upload_str)
--- yaza/qiniu_handler/handler.py
+++ yaza/qiniu_handler/handler.py
@@ -25,46 +25,6 @@ class AlreadyExists(Exception):
         return self.message
 
 
-def upload_image(file_path, bucket, force=False, mime_type="image/png", block_size=65535):
-    data = b""
-    hash_ = hashlib.md5()
-    with open(file_path, "rb") as f:
-        for block in iter(lambda: f.read(block_size), ""):
-            data += block
-            hash_.update(block)
-
-    key = hash_.hexdigest() + os.path.splitext(file_path)[-1]
-
-    return upload_image_str(key, data, bucket, force, mime_type)
-
-
-def upload_image_str(key, data, bucket, force=False, mime_type="image/png"):
-    exists = is_exists(bucket, key)
-    if not force and exists:
-        raise AlreadyExists(bucket, key)
-
-    if exists:
-        delete_file(bucket, key)
-
-    policy = qiniu.rs.PutPolicy(bucket)
-    uptoken = policy.token()
-    extra = qiniu.io.PutExtra()
-    if mime_type:
-        extra.mime_type = mime_type
-
-    data = StringIO(data)
-    ret, err = qiniu.io.put(uptoken, key, data, extra)
-    if err is not None:
-        app.logger.error('error: %s ' % err)
-        raise UploadException(err, key)
-    return u"http://%s.qiniudn.com/%s" % (bucket, key)
-
-
-def upload_file(file_path, bucket, force=False, mime_type=''):
-    return upload_str(file_path, open(file_path, 'rb').read(), bucket, force,
-                      mime_type)
-
-
 def upload_str(key, data, bucket, force=False, mime_type=''):
     qiniu.conf.ACCESS_KEY = app.config["QINIU_CONF"]["ACCESS_KEY"]
     qiniu.conf.SECRET_KEY = app.config["QINIU_CONF"]["SECRET_KEY"]
