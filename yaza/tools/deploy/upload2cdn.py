fe6a91f884572b4d2d834e1dbcc2b8ad770f7ad1
xiechao06 <xiechao06@gmail.com>
2014-08-21 18:24:38 +0800

    make upload to cdn forcely

--- yaza/tools/deploy/upload2cdn.py
+++ yaza/tools/deploy/upload2cdn.py
@@ -10,7 +10,7 @@ def upload_file(file_):
     key = os.path.join('static', os.path.relpath(file_, 'static/dist'))
     print "uploading " + file_ + '...'
     upload_str(key, open(file_).read(),
-                app.config["QINIU_CONF"]["STATIC_BUCKET"])
+                app.config["QINIU_CONF"]["STATIC_BUCKET"], True)
 
 if __name__ == "__main__":
 
