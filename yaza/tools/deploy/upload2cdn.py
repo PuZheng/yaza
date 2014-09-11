bf342e90eb79daeb12fdf6c11a01e6e7337b77e9
xiechao06 <xiechao06@gmail.com>
2014-09-11 14:05:57 +0800

    fix #379

--- yaza/tools/deploy/upload2cdn.py
+++ yaza/tools/deploy/upload2cdn.py
@@ -2,17 +2,27 @@
 
 import sys
 import os
-from yaza.basemain import app
+
+import click
+
+__import__('yaza.basemain')
 from yaza.qiniu_handler import upload_str
 
 
-def upload_file(file_):
+@click.command()
+@click.option('--bucket', help='which bucket to load')
+def upload_files(bucket):
+    """
+    upload files (read from stdin) to qiniu
+    """
+    for l in sys.stdin.xreadlines():
+        upload_file(l.strip().decode('utf-8'), bucket)
+
+
+def upload_file(file_, bucket):
     key = os.path.join('static', os.path.relpath(file_, 'static/dist'))
     print "uploading " + file_ + '...'
-    upload_str(key, open(file_).read(),
-                app.config["QINIU_CONF"]["STATIC_BUCKET"], True)
+    upload_str(key, open(file_).read(), bucket, True)
 
 if __name__ == "__main__":
-
-    for l in sys.stdin.xreadlines():
-        upload_file(l.strip())
+    upload_files()
