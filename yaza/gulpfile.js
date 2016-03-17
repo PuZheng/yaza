6df66e6632134793b992c38df458972b5536bd0c
xiechao06 <xiechoa06@gmail.com>
2016-03-17 15:18:02 +0800

    0

--- yaza/gulpfile.js
+++ yaza/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var spawn = require('child_process').spawn;
  
-gulp.task('build', function () {
+gulp.task('build', ['sass'], function () {
     spawn('r.js', ['-o', 'build.js'], {
         stdio: 'inherit'
     });
